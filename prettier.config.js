e9f0a5fb10fd641ecb7c33b475a628b8eba5fd43
EdgedSquirrels <daniel03044@gmail.com>
2024-02-04 23:02:57 +0800

    feat: add home page

--- prettier.config.js
+++ prettier.config.js
@@ -2,5 +2,6 @@ const styleguide = require('@vercel/style-guide/prettier');
 
 module.exports = {
   ...styleguide,
+  singleQuote: true,
   plugins: [...styleguide.plugins, 'prettier-plugin-tailwindcss'],
 };
